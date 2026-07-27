ba2ef74d9686b09c31d658c25f2659a6bf084d0a
lparisi <l.parisi@epcc.ed.ac.uk>
2026-07-27 14:46:56 +0100

    Cleaned up some of the data

--- anal/reframe/benchmarks.py
+++ anal/reframe/benchmarks.py
@@ -3,6 +3,7 @@ import pandas as pd
 import re
 import seaborn as sns
 import matplotlib.pyplot as plt
+import os
 
 
 def load_reframe_report(filename) -> dict:
@@ -74,4 +75,76 @@ def post_process_cbenchio_table(data: pd.DataFrame) -> pd.DataFrame:
     data_long = data.melt(id_vars=id_columns, value_vars=bandwidth_columns,var_name="replica",value_name="bandwidth") #     # Pivot the bandwidth_x columns
     #sns.pointplot(data=data, x='nodes', y='bandwidth_0')
     data_long = data_long.drop("replica",axis=1)
+
+     # Set defaults parameters 
+    if "stripes" not in data_long.columns:
+        data_long["stripes"] = "1"
+    if "field_size_per_process_per_dimension" not in data_long.columns:
+        data_long["field_size_per_process_per_dimension"] = "1048576"
+
+    # Convert to MiB
+    data_long["field_size_per_process_per_dimension"]=(data_long["field_size_per_process_per_dimension"].astype(int) )
+
+    # Replace the number of stripes with the number of nodes when the number of stripes is set to "num_nodes"
+
+
+    
+    selection = data_long["stripes"] == "num_nodes"
+    data_long.loc[selection, "stripes"] = data_long.loc[selection, "nodes"]
+
+
+    data_long["nodes"] = data_long["nodes"].astype(int)
+    data_long["tasks_per_node"] = data_long["tasks_per_node"].astype(int)
+        
+    data_long["stripes"] = data_long["stripes"].astype(int)
+    data_long["filesystem"] = [get_file_system(base_path) for base_path in data_long["base_path"]]
+
+
     return data_long
+
+
+def get_file_system(path: str) -> str:
+    """Get the file system type for a given path.
+    Args:
+        path (str): The file path to check.
+
+    Returns:
+        str: The file system type ("VAST" or "LUSTRE").
+    """
+    
+    if "vast" in path.split(os.sep):
+        return "vast"
+    else:
+        return "lustre"
+
+def load_cbenchio_reports(filenames):
+    experiments=[]
+    for filename in filenames:
+        data= load_reframe_report(filename)
+        tables=get_performance_tables(data)
+        for test_case, table in tables.items():
+            data = post_process_cbenchio_table(table)
+            experiments.append(data)
+
+           
+    
+    data=pd.concat(experiments).reset_index(drop=True).dropna()
+    return data
+
+
+def get_report_filenames(base_path, pattern="*.json", exclude_pattern=None):
+    """Get a list of report filenames in the specified base path that match the given pattern and do not match the exclude pattern.
+    Args:
+        base_path (str): The base path to search for report files.
+        pattern (str): The pattern to match report files (default: "*.json").
+        exclude_pattern (str): The pattern to exclude report files (default: None).      
+    Returns:
+        list: A list of report filenames that match the given pattern and do not match the exclude pattern.         
+    """
+    report_filenames = []
+    for root, dirs, files in os.walk(base_path):
+        for file in files:
+            for pat in [pattern] if isinstance(pattern, str) else pattern:
+                if re.match(pat, file) and (exclude_pattern is None or not re.match(exclude_pattern, file)):
+                    report_filenames.append(os.path.join(root, file))
+    return report_filenames
